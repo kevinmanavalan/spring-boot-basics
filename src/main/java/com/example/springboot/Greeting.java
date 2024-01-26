420e1885cd8cc53f65db9706d663f5246e687bf9
Kevin Manavalan <kevinseby20@gmail.com>
2024-01-26 12:34:50 +0530

    Initial commit

---
+++ src/main/java/com/example/springboot/Greeting.java
@@ -0,0 +1,5 @@
+package com.example.springboot;
+
+public record Greeting(int age, String message) {
+
+}
---
+++ src/main/java/com/example/springboot/GreetingController.java
@@ -0,0 +1,42 @@
+package com.example.springboot;
+
+import org.springframework.http.HttpStatusCode;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class GreetingController {
+ @GetMapping("/greeting")
+ public Greeting listGreeting(
+         @RequestParam(name = "name", defaultValue = "Kevin") String name,
+         @RequestParam(name = "age", defaultValue = "24") int age
+ )
+ {
+  return new Greeting(age, name);
+ }
+ @PostMapping("/greeting")
+ public String addGreeting(
+         @RequestParam(name = "name") String name,
+         @RequestParam(name = "age", defaultValue = "0") int age)
+ {
+  if(name == null || age == 0){
+   return "Faulty Data";
+  }
+  else{
+   return "Data received successfully";
+  }
+ }
+ @PutMapping("/greeting")
+ public String updateGreeting(
+         @RequestParam(name = "name") String name,
+         @RequestParam(name = "new_name") String new_name
+ )
+ {
+  if(name != null && new_name != null){
+   return "name found and updated";
+  }
+  else{
+   return "name not found";
+  }
+ }
+}
+
